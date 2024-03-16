c4b3e29c69085b84b6180443232b87c35eb4901d
Mohamed Gamal <mg.dev1992@gmail.com>
2024-03-16 22:00:47 +0200

    init service provider

---
+++ src/PackageServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class PackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
