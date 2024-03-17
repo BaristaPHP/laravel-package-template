370b0502f528d2edc3c33d00b97bab7419cd6895
Mohamed Gamal <mg.dev1992@gmail.com>
2024-03-17 20:21:26 +0200

    add factories directory

---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Vendor\PacakgeDatabase\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+class ModelFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
